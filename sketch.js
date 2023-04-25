4a586284f5dd38d6cf19ae37ab46dd683df754ea
vitordev <106778777+vitornine@users.noreply.github.com>
2023-04-25 18:33:48 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,46 +1,49 @@
-var canvas;
-var backgroundImage, bgImg, animal1_img, animal2_img, track;
-var database, gameState;
-var form, player, playerCount;
-var allPlayers, animal1, animal2,bush, grass;
-var animals = [];
-var bushImage, grassImage;
-//BP
-function preload() {
-  backgroundImage = loadImage("./assets/background.jpg");
-  animal1_img = loadImage("../assets/animal1.png");
-  animal2_img = loadImage("../assets/animal2.png");
-  track = loadImage("../assets/track.png");
-  bushImage = loadImage("./assets/bush.png");
- grassImage = loadImage("./assets/grass.png");
-}
+var database;
+var back_img;
+var gameState =0;
+var playerCount = 0;
+var allPlayers;
+var score =0;
+var player, form,game;
+var player1,player2;
+var players;
+var fruits;
+var fruitGroup;
+var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
+var player_img;
+var player1score =0;
+var player2score =0;
 
-//BP
+function preload(){
+  back_img = loadImage("images/jungle.jpg");
+  player_img = loadImage("images/basket2.png");
+  fruit1_img = loadImage("images/apple2.png");
+  fruit2_img = loadImage("images/banana2.png");
+  fruit3_img = loadImage("images/melon2.png");
+  fruit4_img = loadImage("images/orange2.png");
+  fruit5_img = loadImage("images/pineapple2.png");
+  fruitGroup = new Group();
+}
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
+  createCanvas(1000, 600);
   database = firebase.database();
   game = new Game();
   game.getState();
   game.start();
- 
+  
 }
 
-//BP
 function draw() {
-  background(backgroundImage);
-  if (playerCount === 2) {
-    game.update(1);
+  background(back_img);
+  
+   if (playerCount === 2) {
+     game.update(1);
+   }
+   if (gameState === 1) {
+     clear(); 
+     game.play();
+   }
+   if (gameState === 2) {
+   
   }
-
-  if (gameState === 1) {
-    game.play();
-  }
-  if (gameState === 2) {
-    game.showLeaderboard();
-    game.end();
-  }
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
 }
